24983d9a280ab0ddb12dec1f5e9b6e4701746d3a
Tom White <tom@cloudera.com>
2016-12-16 14:38:59 +0000

    Set driver memory

--- q4_spark_eval/test_case_md.sh
+++ q4_spark_eval/test_case_md.sh
@@ -5,12 +5,14 @@ time_gatk() {
   NUM_EXECUTORS=$2
   EXECUTOR_CORES=$3
   EXECUTOR_MEMORY=$4
+  DRIVER_MEMORY=$5
   COMMAND=$(echo $GATK_ARGS | awk '{print $1}')
   LOG=${COMMAND}_$(date +%Y%m%d_%H%M%S).log
   ./gatk-launch $GATK_ARGS \
     -- \
     --sparkRunner SPARK --sparkMaster yarn-client --sparkSubmitCommand spark2-submit \
     --num-executors $NUM_EXECUTORS --executor-cores $EXECUTOR_CORES --executor-memory $EXECUTOR_MEMORY \
+    --driver-memory $DRIVER_MEMORY \
   > $LOG 2>&1
   RC=$?
   DURATION_MINS=$(grep 'Elapsed time' $LOG | grep -Eow "[0-9]+\.[0-9][0-9]")
@@ -19,5 +21,5 @@ time_gatk() {
 
 for num_exec in 64
 do
-  time_gatk "MarkDuplicatesSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/markdups --METRICS_FILE /tmp/markdups-metrics" $num_exec 1 4g >> test_case_md_results.csv
+  time_gatk "MarkDuplicatesSpark -I hdfs:///user/$USER/q4_spark_eval/WGS-G94982-NA12878.bam -O hdfs:///user/$USER/q4_spark_eval/out/markdups" $num_exec 1 4g 4g >> test_case_md_results.csv
 done
